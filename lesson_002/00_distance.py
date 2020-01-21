a39c5c3a105c4527ba50c6a0ca52be6c71436405
Алеша <priymak.a.s@gmail.com>
2020-01-21 22:05:55 +0200

    1 lesson po primeru

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -16,8 +16,20 @@ distances = {}
 
 # TODO здесь заполнение словаря
 
+distances=dict()
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+moscow_london = ((moscow[0]-london[0])**2 + (moscow[1]-london[1])**2)** .5
+moscow_paris = ((moscow[0]-paris[0])**2 + (moscow[1]-paris[1])**2)** .5
+london_paris = ((london[0]-paris[0])**2 + (london[1]-paris[1])**2)** .5
+distances['Moscow']={}
+distances['Moscow'] ['London']= moscow_london
+distances['Moscow'] ['Paris']= moscow_paris
+distances['London']={}
+distances['Moscow'] ['London']= moscow_london
+distances['London'] ['Paris']= moscow_paris
+distances['Paris']={}
+distances['Paris'] ['London']= moscow_london
+distances['Moscow'] ['Paris']= moscow_paris
 print(distances)
-
-
-
-
