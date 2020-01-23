43ac7be2e5393b3bea6c2f85de264d1fc5fa2d82
Алеша <priymak.a.s@gmail.com>
2020-01-23 18:31:55 +0200

    Lesson 2 06 adding summa of a songs

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -21,7 +21,8 @@ violator_songs_list = [
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 # TODO здесь ваш код
-
+tree_song_height= (violator_songs_list[3][1]) + (violator_songs_list[5][1]) + (violator_songs_list[-1][1])
+print("Три песни звучат "+ str(round(tree_song_height,2)) + " минут")
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
     'World in My Eyes': 4.76,
@@ -39,3 +40,5 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+tree_song_height_other= violator_songs_dict['Sweetest Perfection']+violator_songs_dict['Policy of Truth']+ violator_songs_dict['Blue Dress']
+print("А другие три песни звучат "+ str(round(tree_song_height_other,)) + " минут")
