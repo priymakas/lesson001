2f87dcc89e20f874808863054d91d3f20b4006c5
Алеша <priymak.a.s@gmail.com>
2020-02-02 16:16:58 +0200

    try lesson 3 with start home work

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -2,23 +2,42 @@
 
 import simple_draw as sd
 
-sd.resolution = (1200, 600)
+sd.resolution = (1500, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
 # TODO здесь ваш код
+point = sd.get_point(100, 100)
+radius = 50
+for _ in range(3):
+    radius += 5
+    sd.circle(center_position=point, radius=radius, width=3)
+
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
+def buble(point, step, color):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius, width=3, color=sd.random_color())
 
+
+buble(point, step=10, color=sd.random_color())
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
-
+# for x in range(100,1100,100):
+#     point = sd.get_point(x,100)
+#     buble(point=point,step=5)
 # Нарисовать три ряда по 10 пузырьков
 # TODO здесь ваш код
-
+# for x in range(100,1100,100):
+#     for y in range(100,301,100):
+#         point = sd.get_point(x, y)
+#         buble(point=point,step=5)
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
+for _ in range(100):
+    point = sd.random_point()
+    buble(point=point, step=100, color=sd.random_color())
 
 sd.pause()
-
-
--- lesson_003/lecture_snippets/probe.py
+++ lesson_003/lecture_snippets/probe.py
@@ -1,12 +1,24 @@
-x = []
-while x == "4" or "5":
-   x = input("Угадай число  ___ ")
-   if x == "4":
-      print("Удача")
-      break
-   elif x == "5":
-      print("Умничка")
-      break
-   else:
-      print("Не-а")
-print("Ты ввела правильное число и это  " + x)
+# x = []
+# while x == "4" or "5":
+#    x = input("Угадай число  ___ ")
+#    if x == "4":
+#       print("Удача")
+#       break
+#    elif x == "5":
+#       print("Умничка")
+#       break
+#    else:
+#       print("Не-а")
+# print("Ты ввела правильное число и это  " + x)
+
+
+def user():
+
+    """fff"""
+    name = input("Enter name")
+    age = input("Enter age")
+    return name, age
+
+
+result = user()
+print(result)
