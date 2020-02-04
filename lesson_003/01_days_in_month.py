c056a58642484b13c9583714bbaef05b21e72de0
Алеша <priymak.a.s@gmail.com>
2020-02-04 20:38:49 +0200

    make calendar days

--- lesson_003/01_days_in_month.py
+++ lesson_003/01_days_in_month.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+5
 # (if/elif/else)
 
 # По номеру месяца вывести кол-во дней в нем (без указания названия месяца, в феврале 28 дней)
@@ -7,8 +7,50 @@
 # Если номер месяца некорректен - сообщить об этом
 
 # Номер месяца получать от пользователя следующим образом
-user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-print('Вы ввели', month)
+# user_input = input("Введите, пожалуйста, номер месяца: ")
+# month = user_input
+user_input= []
+while user_input == "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "10" or "11" or "12":
+    user_input = input("Введите, пожалуйста, номер месяца: ")
+    if user_input == "1":
+        print('Вы ввели', " 31 day", user_input)
+        break
+    elif user_input == "2":
+        print('Вы ввели', user_input, " 28 day")
+        break
+    elif user_input == "3":
+        print('Вы ввели', user_input, " 31 day")
+        break
+    elif user_input == "4":
+        print('Вы ввели', user_input, " 30 day")
+        break
+    elif user_input == "5":
+        print('Вы ввели', user_input, " 31 day")
+        break
+    elif user_input == "6":
+        print('Вы ввели', user_input, " 30 day")
+        break
+    elif user_input == "7":
+        print('Вы ввели', user_input, " 31 day")
+        break
+    elif user_input == "8":
+        print('Вы ввели', user_input, " 31 day")
+        break
+    elif user_input == "9":
+        print('Вы ввели', user_input, " 30 day")
+        break
+    elif user_input == "10":
+        print('Вы ввели', user_input, " 31 day")
+        break
+    elif user_input == "11":
+        print('Вы ввели', user_input, " 30 day")
+        break
+    elif user_input == "12":
+        print('Вы ввели', user_input, " 31 day")
+        break
+    else:
+        print("Вы ввели некорректный номер месяца")
+        continue
+print("thank")
 
-# TODO здесь ваш код
+    # TODO здесь ваш код
