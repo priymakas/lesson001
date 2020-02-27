58f66918080b29f8561f15ae06cec204f1e5c26b
Алеша <priymak.a.s@gmail.com>
2020-02-27 20:38:56 +0200

    06 hernya

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
+import math
 
+import pygame
 import simple_draw as sd
+point = sd.get_point(50, 100)
+def line(point, color):
+    width = 50
+    for _ in range(3):
+        sd.line(start_point=point, end_point=point, color=sd.COLOR_BLACK, width=4)
 
-rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
-                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+line(point=(50,100),color=sd.COLOR_BLACK)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
